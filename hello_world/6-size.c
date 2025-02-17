82b9fa446f46c8f6f3cab33654975e7fadc8bc35
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-17 14:57:17 +0000

    update 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** 
+ * main : sentences
+ * Return: 0
+ *Sizeof: size of type
+ */
+int main(void)
+{
+printf("Size of a char : %zu octets \n"),sizeof(char);
+printf("Size of an int:%zu octets \n"), sizeof(int);
+printf("Size of a long int: %zu octets \n"), sizeof(long int);
+printf("Size of a long long int:%zu octets \n"), sizeof(long long int);
+printf("Size of a float: %zu octets \n"), sizeof(float);
+return (0);
+}
