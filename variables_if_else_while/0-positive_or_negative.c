7e13e0819814fea90faf3cfb46bd74757a983593
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-17 18:56:23 +0000

    update0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-/** main - Entry point
+
+/**
+ * main - Entry point
  * main function assigns a random number to n.
  * Prints n.
  *
- *Return: always 0 (succes)*/
+ *Return: always 0 (succes)
+*/
+
 /* betty style doc for function main goes there */
 int main(void)
 {
