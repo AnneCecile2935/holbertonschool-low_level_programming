977ef772aff17343fc6e92eccb73e169ae0527b0
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-18 17:30:26 +0000

    update 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *main - Entry point
+ *main function assigns a random number to n
+ *Prints n.
+*
+* Return: always 0 (success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10 ; num++)
+{
+putchar(num + '0');
+if (num < 9)
+putchar(',');
+}
+putchar('$');
+putchar('\n');
+return (0);
+}
