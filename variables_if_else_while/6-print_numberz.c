ceae3f060c1b51f82a9ba131f32d0d150b9b4c56
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-18 14:17:04 +0100

    update 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *main - Entry point
+ *main function assigns a random number to n
+ *Prints n.
+*
+* Return: always 0 (success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+char c;
+for (c = 0; c < 10 ; c++)
+{
+  putchar(c);
+}
+putchar('\n');
+return (0);
+}
