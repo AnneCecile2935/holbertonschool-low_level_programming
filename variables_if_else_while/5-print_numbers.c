ac4fb39575f311da640c57b1b2b5a947031112b3
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-18 10:44:05 +0000

    update 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *main - Entry point
+ *main function assigns a random number to n
+ *Prints n.
+*
+* Return: always 0 (success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+	printf("%d", i);
+}
+return (0);
+}
