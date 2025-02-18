c09a3791e1d7bfae86024de8448843449e09624c
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-18 18:56:31 +0000

    update all

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
 * main - Entry point
 *Return: 0
 */
-
 int main(void)
 {
-	printf("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
 	return (0);
 }
