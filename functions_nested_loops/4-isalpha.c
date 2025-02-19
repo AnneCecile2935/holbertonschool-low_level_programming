6f028e87ea31cd499e825daacbd3e9a29da2ee1c
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-19 13:12:57 +0000

    update all 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _isalpha check if it is an alphanumeric charactere.
+* _isalpha - check if it is an alphanumeric charactere.
 * @c: the charactere to be checked.
 * Return: 1 if it's a lowercase - 0 if it's not a lowercase.
 **/
