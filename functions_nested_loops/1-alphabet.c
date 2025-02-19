a1c70896dfdb4e0cac9113ea74e7ec48b6d837d0
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-19 09:41:32 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 /**
 * main - check the code
 * Return: Always 0.
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
 */
 void print_alphabet(void)
 {
-	char i;
-	for (i= 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
 }
