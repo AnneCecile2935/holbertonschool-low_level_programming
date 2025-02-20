1e52f0d1929d752664c0939b4c21f9e4427588cc
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-20 13:58:52 +0100

    update 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_sign - check is a number is positive or not.
+* _abs - check is a number is positive or not.
 * @n : the number to be chekcked.
 * Return: 1 if it is positive, 0 is it is 0, -1 if it is negative.
 */
