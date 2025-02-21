86ec83a766a65d616ae2c14cc12f47b2533ee6b2
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-21 13:23:12 +0100

    update 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,37 +1,36 @@
 #include "main.h"
-
+/**
+* times_table - create a time table
+*/
 void times_table(void)
 {
-    int i;
-    int j;
-    int r;
-     
-     for ( i = 0; i < 10 ; i++)
-     {
-       for (j = 0; j < 10; j++)
-       {
-         r = i*j;
-	 if(j == 0)
-	   {
-	     _putchar ('0');
-	   }
-	 else
-	   {
-	 _putchar (',');
-	 _putchar (' ');
-	 if (r > 9)
-	   {
-         _putchar((r / 10) + '0');
-	   }
-	 else
-	   {
-	     _putchar (' ');
-	   }
-	 
-	 _putchar((r % 10)+ '0');
-         
-       }
-     }
-       _putchar('\n');
+int i;
+int j;
+int r;   
+for ( i = 0; i < 10 ; i++)
+{
+for (j = 0; j < 10; j++)
+{
+r = i*j;
+if(j == 0)
+{
+_putchar ('0');
+}
+else
+{
+_putchar (',');
+_putchar (' ');
+ if (r > 9)
+{
+_putchar((r / 10) + '0');
+}
+else
+{
+_putchar (' ');
+}
+_putchar((r % 10)+ '0');       
+}
+}
+_putchar('\n');
 }
 }
