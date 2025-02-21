ae06919aa87d359196d880c52d2ff8c5a95e1aea
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-21 08:40:35 +0100

    update 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - return last digit of number
+ * Return: value of the last digit.
+ */
+int print_last_digit(int i)
+{
+  int j;
+  j = i % 10;
+  if (j < 0)
+    {
+      j = -j;
+    }
+  return (0);
+}
