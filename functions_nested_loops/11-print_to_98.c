d4e8b01c8ce0508d23b516df63f88218f4233ffd
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-21 15:32:27 +0100

    update 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include <stdio.h>  
-
 /**
 * print_to_98 - print numbers to 98
 * @n : first number print
@@ -11,16 +10,18 @@ if (n < 98)
 {
 while (n < 98)
 {
-printf ("%d, ", n); n++;
+printf("%d, ", n);
+n++;
 }
 }
 else if (n > 98)
 {
 while (n > 98)
 {
-printf ("%d, ", n); n--;
+printf("%d, ", n);
+n--;
 }
 }
-printf ("%d\n", 98);
+printf("%d\n", 98);
 }
 
