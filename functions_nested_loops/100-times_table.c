a234100b7dd01d5ba0e1361a832144c3b445d093
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-23 11:34:27 +0100

    update 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -17,26 +17,25 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 product = i * j;
-if (j > 0) {
+if (j > 0)
+{
 _putchar(',');
 _putchar(' ');
-}
-if (product < 10) {
-  _putchar(' ');
-  _putchar(' ');
-  _putchar(product +'0');
-}
-else if (product == 0)
-{
-_putchar(product + '0');
-}
-else if (product < 100) {
+if (product < 10)
 _putchar(' ');
+if (product < 100)
+_putchar(' ');
+}
+if (product < 10)
+_putchar(product + '0');
+else if (product < 100)
+{
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
-else {
-_putchar((product / 100) +'0');
+else
+{
+_putchar((product / 100) + '0');
 _putchar(((product / 10) % 10) + '0');
 _putchar((product % 10) + '0');
 }
