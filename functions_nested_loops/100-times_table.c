b455b8bfc1563ad2801752d56e7be8f782102b08
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-23 11:19:27 +0100

    update 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -22,19 +22,20 @@ _putchar(',');
 _putchar(' ');
 }
 if (product < 10) {
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(product +'0');
+  _putchar(' ');
+  _putchar(' ');
+  _putchar(product +'0');
+}
+else if (product == 0)
+{
+_putchar(product + '0');
 }
 else if (product < 100) {
-_putchar(' ');
 _putchar(' ');
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
 else {
-_putchar(' ');
 _putchar((product / 100) +'0');
 _putchar(((product / 10) % 10) + '0');
 _putchar((product % 10) + '0');
