a9576f084cb4cb8e0ea553508873bfdc6e274b4e
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-23 17:40:38 +0100

    update fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - prints first 50 Fibonaci numbers, starting with 1 and 2, separated by comma followed by a space
+* Return : alwyas 0.
+*/
+int main(void)
+{
+int i;
+unsigned long a = 0, b = 1, r;
+for (i = 0; i < 50 ; i++)
+{
+r = a + b ;
+printf("%lu", r);
+a = b;
+b = r;
+if (r == 49)
+printf("\n");
+else
+printf(',');
+}
+return (0);
+}
