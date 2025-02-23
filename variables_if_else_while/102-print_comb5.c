2e00cd31b6d575f4f3f95d3156dc80049577998a
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-23 08:29:29 +0100

    update 12

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,15 @@ int main(void)
 {
 int digit1;
 int digit2;
-
 for (digit1 = 0 ; digit1 <= 99; digit1++)
 {
 for (digit2 = digit1 + 1 ; digit2 <= 99; digit2++)
 {
-  putchar ((digit1 / 10) + '0');
-  putchar ((digit1 % 10) + '0');
-  putchar (' ');
-  putchar ((digit2 / 10) + '0');
-  putchar ((digit2 % 10) + '0');
+putchar ((digit1 / 10) + '0');
+putchar ((digit1 % 10) + '0');
+putchar (' ');
+putchar ((digit2 / 10) + '0');
+putchar ((digit2 % 10) + '0');
 if (digit1 != 99 || digit2 != 99)
   {
 putchar (',');
