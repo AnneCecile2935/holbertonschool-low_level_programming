7053133127dfb2de9b558fdcc76d5f95c3638285
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 18:12:59 +0100

    update 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,17 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
-if ((n % 3 == 0) && (n % 5 ==0))
+if ((n % 3 == 0) && (n % 5 == 0))
 {
-  printf("%s", "FizzBuzz");
+printf("%s", "FizzBuzz");
 }
 else if (n % 3 == 0)
 {
-  printf("%s", "Fizz");
+printf("%s", "Fizz");
 }
 else if (n % 5 == 0)
 {
-  printf("%s", "Buzz");
+printf("%s", "Buzz");
 }
 else
 {
