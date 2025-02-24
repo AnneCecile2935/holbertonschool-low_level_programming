0e0f1085ad2ea7f7bb38175db0528f16536ba100
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 10:13:16 +0100

    update 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * main - check the code.
- * _isupper(c) - function that checkx for uppercase character.
- *@n : character for entry
- * Return: 1 if it's an uppercase, 0 otherwise
+ * _isupper - function that checkx for uppercase character.
+ *@n: parameter for function.
+ * Return: 0
  */
 int _isupper(int c)
 {
