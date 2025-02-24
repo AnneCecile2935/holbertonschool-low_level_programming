7d83188f2e0ef232c2e63ca622c2c31792867881
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 13:47:41 +0100

    update 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - print line if n more than 0
+* @n : parameter of function
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int x;
+for (x = 1; x <= n ; x++)
+{
+_putchar ('_');
+}
+_putchar ('\n');
+}
