aa130e028d9e480213ffb6fd354c72fee7e7c6e3
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 10:52:07 +0100

    update 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isupper - function that checkx for uppercase character.
+* @a: first parameter for function.
+*@second parameter for function.
+* Return: the result
+*/
+int mul(int a, int b)
+{
+  return (a * b);
+}
