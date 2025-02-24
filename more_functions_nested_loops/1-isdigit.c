052a89c75ed73750ee5298a68f3e98ad1e110e80
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 10:36:51 +0100

    update all

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_isdigit - if it is a digit or not
- *@c : parameter of function
- *Return : 1 if true, 0 if false.
- */
+*_isdigit - if it is a digit or not
+*@c: parameter of function
+*Return: 0 .
+*/
 int _isdigit(int c)
 {
 if (c >= '0' && c <= '9')
