c845d3982d6d159d7bec661183f2c2af527e6fcc
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 11:19:50 +0100

    update 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers - function that prints numbers from 0 to 9.
+* Return:0.
+*/
+void print_most_numbers(void)
+{
+int i = 0;
+while(i<=9)
+{
+if ((i != 2) && (i != 4))
+{
+_putchar(i + '0');
+}
+i++;
+}
+_putchar('\n');
+}
+
