ccbbe1c3818a5273c4fac4799c5ace6c057389dc
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-24 10:08:54 +0100

    update 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * _isupper(int c) - function that checkx for uppercase character.
+ * Return: 1 if it's an uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if ('A'<= c && c <= 'Z')
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
