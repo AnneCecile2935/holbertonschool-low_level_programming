fc99f65f539e93af07b2a65761e315c047923001
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-25 09:02:05 +0100

    update the value 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code 
- *@n:value of parameter
- */
+* reset_to_98 - check the code
+*@n:value of parameter
+*/
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
