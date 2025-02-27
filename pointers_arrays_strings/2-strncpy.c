ea6dad6549ab081f7702b2bf9ff4ed3dd3b6fc56
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-27 20:27:54 +0100

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,21 +6,21 @@
 *@n: parameter of byted
 *Return: result of concat
 */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n) // on veut copier src à la suite de dest. n est le nombre de caracteres dans src
 {
-char *ptr = dest;
-while (*src != '\0' && n > 0)
+char *ptr = dest; // je déclare un pointeur vers la chaine dest
+while (*src != '\0' && n > 0) // tant que la chaine src n'est pas nulle ou qu'il reste des caractères à parcourir
 {
-*ptr = *src;
+*ptr = *src; // je renvoie les caracteres de src à dest par mon pointeur ptr
+ptr++; // je continue a parcourir la chaine src avec mon pointeur 
+src++; // je continue de parcourir la chaine src
+n--; // je decremente mon compteur de caracteres
+}
+while (n > 0) // tant que n >0, il reste des caractères a ecrire
+{
+*ptr = '\0'; // on remplit le reste de la chaine avec des caracteres nuls pour eviter des bugs
 ptr++;
-src++;
 n--;
 }
-while (n > 0)
-{
-*ptr = '\0';
-ptr++;
-n--;
-}
-return (dest);
+return (dest); // on retourne la "nouvelle" chaine dest.
 }
