a2b96d5f4a8131b1c13a0a1dc0539c9d77ac31c0
AnneCecile2935 <10435@holbertonstudents.com>
2025-02-27 20:30:37 +0100

    Update 1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -6,7 +6,7 @@
 */
 void swap_int(int *a, int *b)
 {
-int temp = *a;
-*a = *b;
+int temp = *a; // la variable temp permet de stocker temporairement la valeur de a pour ne pas qu'elle soit écrasée une fois le swap fait
+*a = *b; 
 *b = temp;
 }
