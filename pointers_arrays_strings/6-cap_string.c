1a7fde4a5903a0032433e73280a66870159e76cb
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-28 15:33:26 +0100

    update last one

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-*cap_string - capitalize all words of a string 
+*cap_string - capitalize all words of a string
 *@char: pointer to string
 */
-char *cap_string(char*str)
+char *cap_string(char *str)
 {
-char tab [] = {',', ' ', '\t', '\n', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-int i =0;
+char tab [] = {',', ' ', '\t', '\n', ';', '.', '!', '?',
+'"', '(', ')', '{', '}'};
+int i = 0;
 int j;
 if (str[i] >= 'a' && str[i] <= 'z')
 {
@@ -16,7 +17,7 @@ while (str[i] != '\0')
 {
 for (j = 0; tab[j] != '\0'; j++)
 {
-if (str[i] == tab [j] && str[i+1] >= 'a' && str[i+1] <= 'z')
+if (str[i] == tab[j] && str[i+1] >= 'a' && str[i + 1] <= 'z')
 {
 str[i + 1] -= 32;
 }
