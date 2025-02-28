8b856bf603448f7a7604122cc36c6ab0c6b20473
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-28 15:27:01 +0100

    update last one

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*cap_string - capitalize all words of a string 
+*@char: pointer to string
+*/
+char *cap_string(char*str)
+{
+  char tab [] = {',', ' ', '\t', '\n', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int i =0;
+ int j;
+ if (str[i] >= 'a' && str[i] <= 'z')
+   {
+     str[i] -= 32;
+   }
+ while (str[i] != '\0')
+   {
+     for (j = 0; tab[j] != '\0'; j++)
+       {
+	 if (str[i] == tab [j] && str[i+1] >= 'a' && str[i+1] <= 'z')
+	   {
+	     str[i + 1] -= 32;
+	   }
+       }
+     i++;
+   }
+ return (str);
+}
