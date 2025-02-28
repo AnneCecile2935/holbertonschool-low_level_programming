d12508ed3a4b283a80627b29a64bfabd7323741f
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-28 10:22:46 +0100

    update 14

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,13 @@
 void reverse_array(int *a, int n)
 {
 int *start = a;
-int *end = a + (n -1);
+int *end = a + (n - 1);
 while (start < end)
 {
 int temp = *start;
 *start = *end;
 *end = temp;
-start ++;
-end --;
+start++;
+end--;
 }
 }
