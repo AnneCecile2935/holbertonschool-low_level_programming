33cbb76085dd549de13d63fefe3168e88f03ab6a
Annece2935 <anne-cecile.colleter@live.fr>
2025-02-28 09:06:56 +0100

    update 15

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_string_toupper - convert lowercase in uppercase 
+*@*str: pointer to string
+*/
+char *string_toupper(char *str)
+{
+char *ptr = str;
+while (*ptr)
+{
+if (*ptr >= 'a' && *ptr <= 'z')
+{
+*ptr -= ('a' - 'A');
+}
+ptr++;
+}
+return (str);
+}
