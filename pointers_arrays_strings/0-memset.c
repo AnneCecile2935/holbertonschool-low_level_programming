d6c2c7c13738c19d8514a430d3f2dca4ebf5975c
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-03 11:18:38 +0100

    Update 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*memset - The _memset() function fills the first n bytes of the memory
+*_memset - The _memset() function fills the first n bytes of the memory
 *area pointed to by s with the constant byte b
 *@s: pointer to memory
 *@b: value
