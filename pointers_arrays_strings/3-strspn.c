ce88dba87aa0fdd0d9da372872661c8cb979cd3f
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-03 12:40:11 +0100

    update strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_strspn - a function that gets the length of a prefix substring
+*@s: pointer
+*@accept: value
+*Return: s new value of s.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+  unsigned int j;
+  unsigned int i;
+
+  for (j= 0 ;*(s + j); j++)
+    {
+      for ( i = 0; *(accept + i); i++)
+    {
+      if (*(s + j) == *(accept + i))
+      break;
+    }    if (*(accept + i) == '\0')
+	break;  
+}
+      return (j);
+}
