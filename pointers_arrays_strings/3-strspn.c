eb0a3e6e27c7d4b2530aa25f0eeb0492eab1c68a
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-03 12:42:09 +0100

    update strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,17 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int j;
-  unsigned int i;
-
-  for (j= 0 ;*(s + j); j++)
-    {
-      for ( i = 0; *(accept + i); i++)
-    {
-      if (*(s + j) == *(accept + i))
-      break;
-    }    if (*(accept + i) == '\0')
-	break;  
+unsigned int j;
+unsigned int i;
+for (j= 0 ;*(s + j); j++)
+{
+for ( i = 0; *(accept + i); i++)
+{
+if (*(s + j) == *(accept + i))
+break;
 }
-      return (j);
+if (*(accept + i) == '\0')
+break;  
+}
+return (j);
 }
