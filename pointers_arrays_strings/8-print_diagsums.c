f354dbd5ce71c2fe99548e723e1e6bc6a200a7e6
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-03 14:54:41 +0100

    update diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,13 +8,13 @@
 */
 void print_diagsums(int *a, int size)
 {
-  int sum_1 = 0;
-  int sum_2 = 0;
-  int i;
-  for (i = 0; i < size; i++)
-    {
-      sum_1 += a[i*size +i];
-      sum_2 += a[i*size + (size -1 -i)];
-    }
-  printf("%d, %d\n", sum_1, sum_2);
-	 }
+int sum_1 = 0;
+int sum_2 = 0;
+int i;
+for (i = 0; i < size; i++)
+{
+sum_1 += a[i * size + i];
+sum_2 += a[i * size + (size - 1 - i)];
+}
+printf("%d, %d\n", sum_1, sum_2);
+}
