bafd0453b48c8d1e2219d8452c5a73ab317048c4
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-03 15:47:59 +0100

    Update 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,13 +9,13 @@ char *_strpbrk(char *s, char *accept)
 {
 int c;
 int a;
-for (c = 0 ; s[c] != '\0'; c++)
+for (c = 0 ; s[c] != '\0'; c++) // le pointer parcourt la chaine s tant qu'il ne tombe pas sur la fin de la chaine
 {
-for (a = 0; accept[a] != '\0'; a++)
+for (a = 0; accept[a] != '\0'; a++) // pour chaque caract e la chaine s parcouru, on cherche un caract identique dans la chaine accept
 {
-if (s[c] == accept[a])
+if (s[c] == accept[a]) // si c'est le cas
 {
-return (&s[c]);
+return (&s[c]); // on retourne l'adresse du caractère correspondant trouvé dans s
 }
 }
 }
