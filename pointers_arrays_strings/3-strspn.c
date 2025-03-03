314f90ae46ece32170e12d92babb0d50788f07cb
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-03 15:42:13 +0100

    Update 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
 *_strspn - a function that gets the length of a prefix substring
-*@s: pointer
-*@accept: value
+*@s: pointer where we seach the prefix
+*@accept: pointer where the string has characters searching
 *Return: s new value of s.
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int j;
-unsigned int i;
-for (j = 0 ; *(s + j); j++)
+unsigned int j; // indice pour la chaine s
+unsigned int i; // indice pour la chaine accept
+for (j = 0 ; s[j]; j++) // boucle parcourt la chaine s en vérfiiant si s est diff de \0
 {
-for (i = 0; *(accept + i); i++)
+for (i = 0; accept[i]; i++) //boucle parcout chaine accept en vérifiant si caract actuel de s est dans la chaine accept
 {
-if (*(s + j) == *(accept + i))
+if s[j] == accept[i] // si c'est le cas, la boucle d'arrête 
 break;
 }
-if (*(accept + i) == '\0')
+if accept[i] == '\0') // toute la chaîne a été parcourue sans trouver de correspondance
 break;
 }
-return (j);
+return (j); // retourne la valeur de j (nb de carct du début chaine s présents dans chaine accept
 }
