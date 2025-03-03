244c5e36626420c170cdb01db909c6f40440fa6d
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-03 15:26:39 +0100

    Update 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *str = s;
+char *str = s; // pointe vers s
 unsigned int i;
-for (i = 0; i < n; i++)
+for (i = 0; i < n; i++) // parcourt toutes les itérations
 {
-str[i] = (unsigned char)b;
-}
+str[i] = (unsigned char)b; // on accède à chaque élément du tableau ou de la zone mémoire à partir de str
+} // la valeur b est convertie en usigned char pour être sur qu'elle soit traitée en octet
+  // chaque élément de la mémoire est rempli avec la valeur b.
 return (s);
 }
