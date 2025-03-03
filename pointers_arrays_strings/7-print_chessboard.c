9b5816303c54d47e423b5035b68d79a608f4dd95
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-03 14:20:42 +0100

    update chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-*_chessboard - a function that prints the chessboard
+*print_chessboard - a function that prints the chessboard
 *@a: pointer
 */
 void print_chessboard(char (*a)[8])
 {
 int i;
 int j;
-for (i = 0 ;i < 8 ; i++)
+for (i = 0 ; i < 8 ; i++)
 {
-for (j = 0;j < 8 ; j++)
+for (j = 0; j < 8 ; j++)
 {
 _putchar (a[i][j]);
 }
