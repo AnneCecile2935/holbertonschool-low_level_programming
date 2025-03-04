d0cb6b2c376dad26186997b3bcce14aa6fd3c9ca
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-04 10:04:29 +0100

    update all betty

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 *_print_rev_recursion - a function that prints a string in reverse
-*@n: pointer
+*@s: pointer
 */
 void _print_rev_recursion(char *s)
 {
-if( *s == '\0')
+if (*s == '\0')
 {
 return;
 }
