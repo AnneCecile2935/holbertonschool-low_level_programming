251c97ad6910864d554df2a79abd7a2ce33d97ab
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-05 07:18:44 +0100

    update sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
 *_sqrt_recursion - a function that returns the natural square root of number
+*sqrt_square - function for calculate square root of number
 *@n: parameter
 *Return:_sqrt_recursion
 */
 int _sqrt_recursion(int n)
 {
-if( n < 0)
+if (n < 0)
 return (-1);
 return (sqrt_square(n, 0));
 }
 int sqrt_square(int n, int a)
 {
-if (a *a == n)
+if (a * a == n)
 return (a);
 if (a * a > n)
 return (-1);
-return(sqrt_square(n,a + 1));
+return (sqrt_square(n, a + 1));
 }
