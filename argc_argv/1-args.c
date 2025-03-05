35bd48c03c49c675e21fb4fecc6dbdd3b47cfc91
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-05 09:44:17 +0100

    update args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints number of arguments
+* @argc: number of arguments
+* @argv: array of pointers to name of program and arguments
+* Return: Always 0 on success.
+*/
+int main(int argc, char *argv[])
+{
+  (void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
