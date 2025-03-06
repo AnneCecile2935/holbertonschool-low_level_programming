0b1541456a6b03624081449c62b481288295ccf8
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-06 14:57:46 +0100

    Update 3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,13 +6,13 @@
 */
 int factorial(int n)
 {
-if (n < 0)
+if (n < 0) // si n < 0 return -1
 {
 return (-1);
 }
-if (n <= 1)
+if (n <= 1) // pour n<=1 return -1
 {
 return (1);
 }
-return (n * factorial(n - 1));
+return (n * factorial(n - 1)); // pour tous les autres nombre, n * facteur de n-1
 }
