8d0cfca3282f29491a8a1a5f2eece722b86a0e3b
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-07 19:04:23 +0100

    update set

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*set_string - a functino that set string to a char
+*@s: pointer variable
+*@to : pointer variable
+*Return: void (nothing)
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
