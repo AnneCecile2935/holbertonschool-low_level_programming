0aeec6b9996ea081156795a41c921640c65aa763
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-07 10:01:01 +0100

    update change

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -4,7 +4,7 @@
  *main - function principal
  *@argc: counter of arguments
  *@argv: number of arguments
- *Return : always 0
+ *Return: always 0
  */
 int main(int argc, char *argv[])
 {
@@ -13,22 +13,22 @@ int piece[] = {25, 10, 5, 2, 1};
 int num_piece;
 int i;
 if (argc != 2)
-  {
+{
 printf("Error\n");
 return (1);
-  }
+}
 centimes = atoi(argv[1]);
 if (centimes < 0)
-  {
-    printf("0\n");
+{
+printf("0\n");
 return (0);
-  }
+}
 num_piece = 0;
-for (i=0;i < 5; i++)
+for (i = 0; i < 5; i++)
 {
 num_piece += centimes / piece[i];
 centimes %= piece[i];
- }
+}
 printf("%d\n", num_piece);
 return (0);
 }
