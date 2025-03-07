d905fe9f335ab8d57341144f6e1704f81584de29
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-07 12:59:53 +0100

    update palindrome

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* is_palindrome_p - a function.
+*is_palindrome_p - a function.
 *@debut : parameter debut chaine
 *@fin :parameter fin chaine
 *Return:is_palindrome_p
+*
 *is_palindrome - a function that returns 1 if a string is a palindrome
 *and 0 if not
 *@s: parameter for string
