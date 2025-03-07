3ad6e7200ba01161200a9526d600123dbf6719c6
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-07 13:01:13 +0100

    update palindrome

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -4,11 +4,6 @@
 *@debut : parameter debut chaine
 *@fin :parameter fin chaine
 *Return:is_palindrome_p
-*
-*is_palindrome - a function that returns 1 if a string is a palindrome
-*and 0 if not
-*@s: parameter for string
-*Return : is_palindrome
 */
 int is_palindrome_p(char *s, int debut, int fin)
 {
@@ -22,6 +17,12 @@ return (0);
 }
 return (is_palindrome_p(s, debut + 1, fin - 1));
 }
+/**
+*is_palindrome - a function that returns 1 if a string is a palindrome
+*and 0 if not
+*@s: parameter for string
+*Return : is_palindrome
+*/
 int is_palindrome(char *s)
 {
 int length = 0;
