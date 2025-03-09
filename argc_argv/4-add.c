a0548660d991266e567a87d33a97c9bc5c803345
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-09 16:06:49 +0100

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,15 +9,15 @@
 int is_number(char *str)
 {
 int i = 0;
-if (str[i] == '\0')
+if (str[i] == '\0') // si i est nulle return 0
 return (0);
-while (str[i])
+while (str[i]) //continue
 {
-if (!isdigit(str[i]))
-return (0);
-i++;
+if (!isdigit(str[i])) // si i n'est pas un chiffre
+return (0); // return 0
+i++; // continue de parcourir
 }
-return (1);
+return (1); //retourne un pour tous les autres cas
 }
 /**
 *main - prints number of arguments
@@ -28,15 +28,15 @@ return (1);
 int main(int argc, char *argv[])
 {
 int sum = 0, i;
-for (i = 1; i < argc; i++)
+for (i = 1; i < argc; i++) // pour i < au nombre d'arguments
 {
-if (!is_number(argv[i]))
+if (!is_number(argv[i])) // est-ce que c'est un nombre valide
 {
 printf("Error\n");
 return (1);
 }
-sum += atoi(argv[i]);
+sum += atoi(argv[i]); // ajoute la valeur de l'arg à somme
 }
-printf("%d\n", sum);
+printf("%d\n", sum); // affiche la somme
 return (0);
 }
