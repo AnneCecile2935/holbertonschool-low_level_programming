31ac01fdd215c0eb7675aadb3a2d1fb7214e912a
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-10 13:38:34 +0100

    update alloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+*alloc_grid - return the lenght of a string a pointer to 2 dimensionnal array
+*integer
+*@width : parameter function
+*@height: parameter function
+*Return: return the result , the length.
+*/
 int **alloc_grid(int width, int height)
+{
+int ** grid;
+int i;
+int j;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+grid[i][j] = 0;
+}
+return (grid);
+}
