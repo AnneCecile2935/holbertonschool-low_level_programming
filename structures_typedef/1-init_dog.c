400e97412744b57659c3b5a84b5efac5d98c04f4
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-11 16:11:32 +0100

    update init dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,17 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  *init_dog - function initialize variable of struct
  *@d: parameter struct
  *@name: parameter
  *@age: parameter
  *@owner: parameter
+ *Return: Nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+  if (d == NULL)
+    d = malloc(sizeof(struct dog));
 d->name = name;
 d->age = age;
 d->owner = owner;
