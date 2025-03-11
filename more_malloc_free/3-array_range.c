a2342572d828debe38ac4b56244158e522b7a2b2
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-11 13:59:23 +0100

    update array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,22 +9,18 @@
  */
 int *array_range(int min, int max)
 {
-    int *ptr;
-    int i;
-    int size;
-
-    if (min > max)
-        return (NULL);
-
-    size = max - min + 1;
-    ptr = malloc(size * sizeof(int));
-    if (ptr == NULL)  /* Always check if malloc succeeded */
-        return (NULL);
-
-    for (i = 0; i < size; i++)
-    {
-        ptr[i] = min + i;
-    }
-
-    return (ptr);
+int *ptr;
+int i;
+int size;
+if (min > max)
+return (NULL);
+size = max - min + 1;
+ptr = malloc(size * sizeof(int));
+if (ptr == NULL)  /* Always check if malloc succeeded */
+return (NULL);
+for (i = 0; i < size; i++)
+{
+ptr[i] = min + i;
+}
+return (ptr);
 }
