19b010a96509c6af613eef3161769b8f58c8f6c2
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-11 13:46:57 +0100

    update array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *array_range - function create an array of integer 
+ *@min: parameter min
+ *@max: parameter max
+ *Return:ptr pointeur
+ */
+int *array_range(int min, int max)
+{
+int *ptr;
+int i;
+int size;
+if (min > max)
+return (NULL);
+size = max - min + 1;
+ptr = malloc(size * sizeof(int *));
+for (i = 0; i< size; i++)
+{
+ptr[i] = min + i;
+}
+return (ptr);
+}
