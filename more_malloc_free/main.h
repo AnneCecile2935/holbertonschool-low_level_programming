1b849711ee8ae75d3802c0ed3682420dd7c7d8ea
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-11 11:00:26 +0100

    update calloc

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,2 +1,4 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
