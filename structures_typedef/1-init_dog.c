eda1715a5e866331abfff22c3d3a8ba991a337df
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-16 09:39:31 +0100

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,9 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
-d->age = age;
-d->owner = owner;
+if (d == NULL) // par sécurité on vérifie que le pointeur travaille avec une vraie structure en mémoire
+d = malloc(sizeof(struct dog)); // on créer l'allocation de mémoire pour la nouvelle structure
+d->name = name; // reçoit l'adresse de la chaine name
+d->age = age; //  la valeur age
+d->owner = owner; // reçoit la valeur de la chaine owner
 }
