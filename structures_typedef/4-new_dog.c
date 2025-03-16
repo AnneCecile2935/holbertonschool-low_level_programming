d8c0cac58d01087cb8e6cfd03232433d8bc9f947
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-16 09:57:24 +0100

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,28 +2,6 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *_strcpy - function that copy a string
- *@src: parameter struct
- *Return: dest, pointer
- */
-char *_strcpy(const char *src)
-{
-char *dest;
-int i = 0;
-int len = 0;
-while (src[len] != '\0')
-len++;
-dest = malloc(sizeof(char) * (len + 1));
-if (dest == NULL)
-return (NULL);
-for (i = 0; i < len; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = '\0';
-return (dest);
-}
-/**
  *new_dog - create a new struct
  *@name: parameter struct
  *@age:parameter
@@ -32,22 +10,37 @@ return (dest);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
-dog = (dog_t *)malloc(sizeof(dog_t));
+dog_t *dog; //pointeur vers la struture dog_t qui va contenir les infos de dog
+name_len = 0; //  longueur name
+owner_len = 0; // longueur owner
+dog = (dog_t *)malloc(sizeof(dog_t)); // aloocation de mémoire structure
 if (dog == NULL)
 return (NULL);
-dog->name = _strcpy(name);
+while (name[name_len] != '\0') // calcul longueur de name
+name_len++;
+dog->name = malloc(sizeof(char) * (name_len + 1)); //allocation de mémoire pour name
 if (dog->name == NULL)
 {
 free(dog);
 return (NULL);
 }
-dog->owner = _strcpy(owner);
+for (i = 0; i < name_len; i++) // on copie chaque caract de name dans dog -> name et la valeur nulle
+{
+dog->name[i] = name[i];
+dog->name[i] = '\0';
+}
+while (owner[owner_len] != '\0')
+owner_len++;
+dog->owner = malloc(sizeof(char) * (owner_len + 1));
 if (dog->owner == NULL)
 {
-free(dog->name);
 free(dog);
 return (NULL);
+}
+for (i = 0; i < owner_len; i++)
+{
+dog->owner[i] = owner[i];
+dog->owner[i] = '\0';
 }
 dog->age = age;
 return (dog);
