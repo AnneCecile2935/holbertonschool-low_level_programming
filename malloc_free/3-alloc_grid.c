d794bbe562a773cd51b7bd0f04d223d05e4b240e
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-16 09:17:28 +0100

    Update 3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,30 +9,30 @@
 */
 int **alloc_grid(int width, int height)
 {
-int **grid;
+int **grid; // double pointer tableau 2D
 int i;
 int j;
-if (width <= 0 || height <= 0)
+if (width <= 0 || height <= 0) // vérifier si width et height ne sont pas nulles
 {
 return (NULL);
 }
-grid = malloc(height * sizeof(int *));
-if (grid == NULL)
+grid = malloc(height * sizeof(int *)); // allocation mémoire pour height lignes
+if (grid == NULL) // verifier que c'est pas nul
 {
 return (NULL);
 }
-for (i = 0; i < height; i++)
+for (i = 0; i < height; i++) // on alloue de la mémoire colonne pour chaque ligne height
 {
 grid[i] = malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-for (j = 0; j < i; j++)
-free(grid[j]);
-free(grid);
+for (j = 0; j < i; j++) // si l'alllocation fail, on libère
+free(grid[j]); // toutes les lignes déjà allouées
+free(grid); // on libère le tableau
 return (NULL);
 }
-for (j = 0; j < width; j++)
-grid[i][j] = 0;
+for (j = 0; j < width; j++) // pour chaque allocation de mémoire
+grid[i][j] = 0; // initialisation de chaque element à 0
 }
 return (grid);
 }
