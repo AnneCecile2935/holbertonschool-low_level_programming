462f3d8734954854de037379ca25a7497776a008
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 14:16:03 +0100

    update int index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,8 +1,24 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  *int_index - function that searches for an integer
  *@array : pointer to array
  *@size : parameter size of the array
  *@cmp : pointeur cmp
+ *Return: -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL || cmp == NULL)
+return (-1);
+if (size <= 0)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
+
