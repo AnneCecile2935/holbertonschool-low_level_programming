b890b41ea3b2d84880a4d2967ed23fc6bf8b9dd9
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 11:54:32 +0100

    update create array iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,9 +3,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
   int i ;
+  if (array == NULL || action == NULL)
+    return NULL;
+  
   for (i = 0; i < size; i++)
     {
-      array[i] = array_iterator(action);
+      array[i] = action(i);
     }
   action(array[i])
     
