ceded1e2dbe3d230f03a3f762ccd5e0b7c1e1b5f
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 15:32:35 +0100

    update get op func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,20 @@
 #include "3-calc.h"
+/**
+ *get_op_func - function select the correct function to perform the operation
+ *asked by the user.
+ *@s : pointer to op
+ *Return: the good function
+ */
 int (*get_op_func(char *s))(int, int)
 {
-  
+int i;
+while (ops[i].op != NULL)
+{
+if (strcmp(s, ops[i].op == 0)
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
+}
