b7389bbf7c90f72c383c994dd6eda09045ec77c4
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 08:54:12 +0100

    update print name

---
+++ function_pointer/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - function print name
+ *@name: parameter pointer
+ *@f : function
+ *Return : nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == 0 || f == 0)
+return;
+f(name);
+}
