5ad8e533b0d825c60392abac0895f3c5f5f174b0
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 18:07:41 +0100

    update all right

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,6 +23,11 @@ exit(98);
 num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
+if ((op[0] == '/' || op[0] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
 operator = get_op_func(op);
 if (operator == NULL)
 {
