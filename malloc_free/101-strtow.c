b4669174f20ae9347c4d58166654c0db427d9e9a
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 21:41:47 +0100

    update

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+char **strtow(char *str)
+{
+  if (str == NULL || str == '\0')
+    return (NULL);
+  while (str[i] != '\0')
+    {
+      if (str[i] == ' ')
+      ptr[i] = str[i];
+  i++;
+    }
+  return (ptr);
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -5,3 +5,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
