04c730fc6dad61bd8f59e9656e0f80845d606666
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-17 12:11:51 +0100

    update array good with betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,14 +1,20 @@
+#include <stddef.h>
 #include "function_pointers.h"
-
+/**
+ *array_iterator - function that executes a function given as a parameter
+ *on each element of an array
+ *@size : parameter size of the array
+ *@action : pointer to function
+ *@array : pointer to array
+ *Return : nothing
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  int i ;
-  if (array == NULL || action == NULL)
-    return NULL;
-  
-  for (i = 0; i < size; i++)
-    {
-      array[i] = action(i);
-    }
-  action(array[i])
-    
+size_t i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
