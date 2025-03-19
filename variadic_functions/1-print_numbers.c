ec9e6b5c23ccbfc7ce45a4bc49fd30ac5866f9d2
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-19 08:53:02 +0100

    Update 1-print_numbers.c with commentary

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,23 +12,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i = 0;
 int current_number;
 va_list arg;
-if (n == 0)
+if (n == 0) // check if n==0 print a /n
 {
 printf("\n");
 return;
 }
-if (separator == 0)
+if (separator == 0) // check if separator =0 , print nothing
 separator = "";
-va_start(arg, n);
-for (i = 0; i < n; i++)
+va_start(arg, n); // initialize to read arguments
+for (i = 0; i < n; i++) // loops to cath the number
 {
-current_number = va_arg(arg, int);
-printf("%d", current_number);
-if (i < (n - 1) && separator != NULL)
+current_number = va_arg(arg, int); // catch the number
+printf("%d", current_number); // print the number
+if (i < (n - 1) && separator != NULL) // add separator without the last number prints and check is not null
 {
-printf("%s", separator);
+printf("%s", separator); // print separator
 }
 }
-printf("\n");
-va_end(arg);
+printf("\n"); // print /n
+va_end(arg); // free va list
 }
