b81a3a25c5a0e95a974a739a8c7f14a107fea544
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-19 08:54:48 +0100

    Update 0-sum_them_all.c with commentary

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 unsigned int i = 0;
 int sum = 0;
 va_list arg;
-if (n == 0)
+if (n == 0) // check n is not 0 to continue
 return (0);
-va_start(arg, n);
-for (i = 0; i < n; i++)
+va_start(arg, n); // initialize to read arguments
+for (i = 0; i < n; i++) // loop to catch each n 
 {
-sum += va_arg(arg, int);
+sum += va_arg(arg, int); // sum of n arguments
 }
-va_end(arg);
-return (sum);
+va_end(arg); // free va_list
+return (sum); // return result of sum
 }
