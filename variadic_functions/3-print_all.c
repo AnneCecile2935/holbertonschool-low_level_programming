9d3ea983e42c523bfa19814531a4224e4c7e4ad9
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-21 10:27:23 +0100

    update finish task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,18 +2,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- *print_strings - function that prints numbers, followed by a new line
- *@separator : parameter for separators
- *@n : parameter if numbers
- *Return: nothing.
- */
-/**
  * print_letter - function to recognize letter
  *@arg: the argument in parameter
- *@va_list: parameter list
  *Return: nothing
  */
-print_letter(va_list,arg)
+void print_letter(va_list arg)
 {
 char letter = va_arg(arg, int);
 printf("%c", letter);
@@ -21,21 +14,19 @@ printf("%c", letter);
 /**
  * print_flo - function to recognize float
  *@arg: the argument in parameter
- *@va_list: parameter list
  *Return: nothing
  */
-print_flo(va_list, arg)
+void print_flo(va_list arg)
 {
 double flo = va_arg(arg, double);
-printf("%.2f",flo);
+printf("%.2f", flo);
 }
 /**
  * print_number - function to recognize int
  *@arg: the argument in parameter
- *@va_list: parameter list
  *Return: nothing
  */
-print_number(va_list, arg)
+void print_number(va_list arg)
 {
 int number = va_arg(arg, int);
 printf("%d", number);
@@ -43,21 +34,54 @@ printf("%d", number);
 /**
  * print_str - function to recognize string
  *@arg: the argument in parameter
- *@va_list: parameter list
  *Return: nothing
  */
-print_str(va_list, arg)
+void print_str(va_list arg)
 {
+char *str = va_arg(arg, char*);
 if (str == NULL)
+{
 printf("(nil)");
-char str = va_arg(arg, int);
+return;
+}
 printf("%s", str);
 }
+/**
+ *print_all - function prints all type of format
+ *@format: parameter
+ *Return: nothing.
+  */
 void print_all(const char * const format, ...)
 {
-  va_list arg;
-  va_start(arg, format);
-  while (*format !='\0')
-    {
-      
+
+print type[] = {
+{'c', print_letter},
+{'i', print_number},
+{'f', print_flo},
+{'s', print_str},
+{'\0', NULL},
+};
+char *separator = "";
+int i = 0;
+int j;
+va_list arg;
+va_start(arg, format);
+while (format && format[i])
+{
+j = 0;
+while (type[j].entry)
+{
+if (format[i] == type[j].entry)
+{
+printf("%s", separator);
+type[j].print_func(arg);
+separator = ", ";
+break;
+}
+j++;
+}
+i++;
+}
+va_end(arg);
+printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct print - Structure charactere to the function
+ * @entry: symbole du type 
+ * @print_func: poiter to the function
+ */
+typedef struct print
+{
+char entry;
+void (*print_func)(va_list);
+} print;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
