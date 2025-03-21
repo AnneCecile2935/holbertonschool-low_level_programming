aa6ffac534a4a2d41ff793503eaaa64cd2619e2a
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-21 13:15:43 +0100

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,28 +13,28 @@ int main(int argc, char *argv[])
 int num1;
 int num2;
 int calc;
-int (*operator)(int, int);
-char *op;
-if (argc != 4)
+int (*operator)(int, int); // définir le pointeur sur fonction
+char *op; // définir un pointeur char
+if (argc != 4) // verifier que les parametres en entrée sont bon (il en faut 4)
 {
 printf("Error\n");
 exit(98);
 }
-num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[3]);
-if ((op[0] == '/' || op[0] == '%') && num2 == 0)
+num1 = atoi(argv[1]); // convertir le premier num doit être convertit en char avec atoi argv 
+op = argv[2]; // chercher l'oprateur avec un pointeur op via argv
+num2 = atoi(argv[3]); // convertir le deuxième num via argv
+if ((op[0] == '/' || op[0] == '%') && num2 == 0) // faire une boucle if si l'opérateur est / ou % et que le num2 == 0 
 {
-printf("Error\n");
+printf("Error\n"); // imprimer error
 exit(100);
 }
-operator = get_op_func(op);
-if (operator == NULL)
+operator = get_op_func(op); // appel du pointeur de fonction operator
+ if (operator == NULL) // s(il est NULL
 {
-printf("Error\n");
+printf("Error\n"); // afficher erreur 
 exit(99);
 }
-calc = operator(num1, num2);
-printf("%d\n", calc);
+calc = operator(num1, num2); // calculer le résultat
+printf("%d\n", calc); // affciher le résultat
 return (0);
 }
