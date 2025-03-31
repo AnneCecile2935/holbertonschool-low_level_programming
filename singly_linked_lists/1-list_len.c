7d1aa9c712ff141829fbc768a3512861f195a623
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-31 11:31:43 +0200

    fisrt version of task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - Prints all the elements of a linked list
+ * @h: Pointer to the head of the list
+ *
+ * Description: This function traverses a singly linked list,
+ * prints each node's string and length, and returns the
+ * total number of nodes.
+ *
+ * Return: The number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+unsigned int count = 0;
+const list_t *current = h;
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return (count);
+}
