cdf3f46b950d6db09e2b9998df3517aec23b4a2c
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-31 12:39:00 +0200

    update task 2

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,6 +16,8 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 int _putchar(char c);
+int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif /* LIST_H */
