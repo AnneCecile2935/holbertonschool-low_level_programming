564daa3538f5c49d459eb62c53dfc1077c36301a
AnneCecile2935 <10435@holbertonstudents.com>
2025-03-31 15:02:04 +0200

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,28 +10,28 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *node_end;
-list_t *parc = *head;
+list_t *node_end; // pointeur du dernier noeud
+list_t *parc = *head; // pointeur qui va parcourir la list et démarre à head
 unsigned int len = 0;
-if (str == NULL)
+if (str == NULL) // verif si la chaine n'est pas nulle
 return (NULL);
-while (str[len])
+while (str[len]) // compte la taille de la chaine
 len++;
-node_end = malloc(sizeof(list_t));
-if (node_end == NULL)
+node_end = malloc(sizeof(list_t)); // alloue mémoire pour le noeud
+if (node_end == NULL) // verifie s'il n'est pas null
 return (NULL);
-node_end->str = strdup(str);
-node_end->len = len;
-node_end->next = NULL;
-if (*head == NULL)
+node_end->str = strdup(str); // on duplique la chaine str avec strdup et on le copie dans node_end
+node_end->len = len; // définir la taille du noeud
+node_end->next = NULL; // le noeud suivant sera le NULL
+if (*head == NULL) // si on arrive à la fin de la liste
 {
-*head = node_end;
-return (node_end);
+*head = node_end; // on "place le noeud"
+return (node_end); // retour du noeud
 }
-while (parc->next)
+while (parc->next) // si la liste est déjà "prise"
 {
-parc = parc->next;
+parc = parc->next; // on parcourt le noeud suivant
 }
-parc->next = node_end;
-return (node_end);
+parc->next = node_end; // on place notre noeud de fin après
+return (node_end); // on retourne le noeud
 }
