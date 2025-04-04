bc951585cd808ad5f432bd094da347deaa4b693a
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-04 07:14:51 +0200

    task 0 projet 3

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * read_textfile - function to read and print the text
  * @filename: the name of file to read
@@ -8,16 +10,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE* fp = fopen("Requiescat", "r");
-    char buffer[1024];
-    ssize_t total_read = 0;
-    if (fp !=NULL)
+   int fd;
+   ssize_t bytes_written;
+    char *buffer;
+    ssize_t bytes_read;
+    if (filename == NULL)
          return (0);
-    while (fgets (buffer, sizeof(buffer) -1, fp) != NULL)
-            {
-                printf("%s", buffer);
-                total_read += strlen(buffer);
-            }
-    fclose(fp);
-    return (total_read);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    return (0);
+    buffer = malloc(sizeof(char)*(letters + 1 ));
+    if (buffer == NULL)
+    {
+        close (fd);
+        return (0);
+    }
+    bytes_read = read (fd, buffer, letters);
+    if( bytes_read == -1)
+    {
+        free (buffer);
+        close (fd);
+        return (0);
+    } 
+    buffer [bytes_read] = '\0';
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+{
+    free(buffer);
+    close(fd);
+    return (0);
+}
+return (bytes_written);
 }
