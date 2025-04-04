cc88573218f56a8f76707e53968ac0cf84df982f
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-04 12:44:59 +0200

    update task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <sys/stat.h>
 /**
  * error_check - checks for errors
- * @fdfr: integer returned from read
- * @fdto: integer returned from write
- * @av: argument element
+ * @fd_src: integer returned from read
+ * @fd_dest: integer returned from write
+ * @argv: argument element
  */
 void error_check(int fd_src, int fd_dest, char **argv)
 {
@@ -20,45 +21,44 @@ void error_check(int fd_src, int fd_dest, char **argv)
 		exit(99);
 	}
 }
+/**
+ * close_check - function to close if error 100
+ * @fd : parameter
+ * Return : nothing
+ */
 void close_check(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
 }
 /**
  * main - Entry point
- * @ac: arguments
- * @av: arguments elements
+ * @argc: arguments
+ * @argv: arguments elements
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-	/* Declare variables */
-	int numread, num_writ, fd_src, fd_dest, clo_src, clo_dest;
+	int numread, num_writ, fd_src, fd_dest;
 	char buffer[1024];
-	/* check if arguments are correct */
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	/* open and read the file */
-	fd_src = open(argv[1], O_RDONLY);
-	/* open and wrtie to file_to */
-	fd_dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	/* check for error */
-	error_check(fd_src, fd_dest, argv);
 	struct stat st;
-    if (stat(argv[2], &st) == -1)
-        chmod(argv[2], 0664);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+	fd_src = open(argv[1], O_RDONLY);
+	fd_dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	error_check(fd_src, fd_dest, argv);
+if (stat(argv[2], &st) == -1)
+chmod(argv[2], 0664);
 	while ((numread = read(fd_src, buffer, 1024)) > 0)
 	{
 		num_writ = write(fd_dest, buffer, numread);
-		/* check for error */
-		if (num_writ == -1 || num_writ != numread)
+			if (num_writ == -1 || num_writ != numread)
 			{
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 				close_check(fd_src);
@@ -73,9 +73,7 @@ int main(int argc, char **argv)
 				close_check(fd_dest);
 				exit(98);
 	}
-	
 	close_check(fd_src);
-	close_check (fd_dest);
-	
+	close_check(fd_dest);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -8,5 +8,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_check(int fdfr, int fdto, char **av);
+void error_check(int fd_str, int fd_dest, char **argv);
+void close_check(int fd);
 #endif /* MAIN_H */
