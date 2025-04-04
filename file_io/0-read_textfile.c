a21424709604ca708b1cacb9122a3f6e1770680e
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-04 07:24:39 +0200

    update file read task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,38 +7,44 @@
  * read_textfile - function to read and print the text
  * @filename: the name of file to read
  * @letters : the number of letter to read
+ * Return: number bytes write
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-   int fd;
-   ssize_t bytes_written;
-    char *buffer;
-    ssize_t bytes_read;
-    if (filename == NULL)
-         return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    return (0);
-    buffer = malloc(sizeof(char)*(letters + 1 ));
-    if (buffer == NULL)
-    {
-        close (fd);
-        return (0);
-    }
-    bytes_read = read (fd, buffer, letters);
-    if( bytes_read == -1)
-    {
-        free (buffer);
-        close (fd);
-        return (0);
-    } 
-    buffer [bytes_read] = '\0';
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
+int fd;
+ssize_t bytes_written;
+char *buffer;
+ssize_t bytes_read;
+ssize_t total_written = 0;
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
 {
-    free(buffer);
-    close(fd);
-    return (0);
+close(fd);
+return (0);
+}
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+while (total_written < bytes_read)
+{
+buffer [bytes_read] = '\0';
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+total_written += bytes_written;
 }
 return (bytes_written);
 }
