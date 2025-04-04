6bc453dcbce86b8db94868bf018c9bac7ab60208
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-04 07:43:17 +0200

    update function task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,7 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
-ssize_t bytes_written;
 char *buffer;
 ssize_t bytes_read;
 ssize_t total_written = 0;
@@ -21,30 +20,15 @@ return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buffer = malloc(sizeof(char) * (letters + 1));
+buffer = malloc(letters);
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
 bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-{
+total_written = write(STDOUT_FILENO, buffer, bytes_read);
 free(buffer);
 close(fd);
-return (0);
-}
-buffer [bytes_read] = '\0';
-while (total_written < bytes_read)
-{
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-free(buffer);
-close(fd);
-return (0);
-}
-total_written += bytes_written;
-}
 return (total_written);
 }
