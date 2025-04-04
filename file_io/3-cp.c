eeb67b11cdecb889b66130cb979a5ae1955ad784
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-04 12:35:21 +0200

    update task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,20 +5,28 @@
  * @fdto: integer returned from write
  * @av: argument element
  */
-void error_check(int fdfr, int fdto, char **av)
+void error_check(int fd_src, int fd_dest, char **argv)
 {
 	/* check if the file was read */
-	if (fdfr == -1)
+	if (fd_src == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	/* check if file was written */
-	if (fdto == -1)
+	if (fd_dest == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+}
+void close_check(int fd)
+{
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
 }
 /**
  * main - Entry point
@@ -26,43 +34,48 @@ void error_check(int fdfr, int fdto, char **av)
  * @av: arguments elements
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	/* Declare variables */
-	int numread = 1024, numwrit, fdfr, fdto, clfr, clto;
-	char buf[1024];
+	int numread, num_writ, fd_src, fd_dest, clo_src, clo_dest;
+	char buffer[1024];
 	/* check if arguments are correct */
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	/* open and read the file */
-	fdfr = open(av[1], O_RDONLY);
+	fd_src = open(argv[1], O_RDONLY);
 	/* open and wrtie to file_to */
-	fdto = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	fd_dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	/* check for error */
-	error_check(fdfr, fdto, av);
-	while (numread > 0)
+	error_check(fd_src, fd_dest, argv);
+	struct stat st;
+    if (stat(argv[2], &st) == -1)
+        chmod(argv[2], 0664);
+	while ((numread = read(fd_src, buffer, 1024)) > 0)
 	{
-		/* read and give the number of char read */
-		numread = read(fdfr, buf, 1024);
+		num_writ = write(fd_dest, buffer, numread);
 		/* check for error */
-		if (numread == -1)
-			error_check(-1, 0, av);
-		/* write to the file */
-		numwrit = write(fdto, buf, numread);
-		/* check for error */
-		if (numwrit == -1)
-			error_check(0, -1, av);
+		if (num_writ == -1 || num_writ != numread)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close_check(fd_src);
+				close_check(fd_dest);
+				exit(99);
+			}
 	}
-	clfr = close(fdfr);
-	/* check if file descriptor for read closes */
-	if (clfr == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdfr), exit(100);
-	/* check if file descriptor for write closes */
-	clto = close(fdto);
-	if (clto == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdto), exit(100);
+	if (numread == -1)
+	{
+		error_check(0, -1, argv);
+		close_check(fd_src);
+				close_check(fd_dest);
+				exit(98);
+	}
+	
+	close_check(fd_src);
+	close_check (fd_dest);
+	
 	return (0);
 }
